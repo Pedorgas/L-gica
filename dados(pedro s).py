831f8d7e6e721cc1f268c12c01194a84010bc2cc
Pedorgas <50590716+Pedorgas@users.noreply.github.com>
2019-05-13 14:16:44 -0300

    sei la

--- dados(pedro s).py
+++ dados(pedro s).py
@@ -5,11 +5,11 @@ print("jogo do dado")
 
 dado1 = randint(1, 6)   # numero aleatorio entre 1 e 6
 dado2 = randint(1, 6)   # numero aleatorio entre 1 e 6
-print("Dado 1=", dado1 + dado2)
+print('Dado 1=', dado1 + dado2)
 
 dado3 = randint(1, 6)   # numero aleatorio entre 1 e 6
 dado4 = randint(1, 6)   # numero aleatorio entre 1 e 6
-print("Dado 3=", dado3 + dado4)
+print('Dado 3=', dado3 + dado4)
 
 P1 = dado1+dado2
 
