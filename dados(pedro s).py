9b0bc249a49546d6173632b0cbfcf6a34afd5b97
Pedorgas <50590716+Pedorgas@users.noreply.github.com>
2019-05-13 13:58:08 -0300

    mais comentario

--- dados(pedro s).py
+++ dados(pedro s).py
@@ -3,12 +3,12 @@ from random import randint  # Importa a funcao de gerar numeros aleatorios
 print("hamburger")
 print("jogo do dado")
 
-dado1=randint(1,6)
-dado2=randint(1,6)
+dado1=randint(1,6)   # numero aleatorio entre 1 e 6
+dado2=randint(1,6)   # numero aleatorio entre 1 e 6
 print("Dado 1=",dado1 + dado2)
 
-dado3=randint(1,6)
-dado4=randint(1,6)
+dado3=randint(1,6)   # numero aleatorio entre 1 e 6
+dado4=randint(1,6)   # numero aleatorio entre 1 e 6
 print("Dado 3=",dado3 + dado4)
 
 P1=dado1+dado2
