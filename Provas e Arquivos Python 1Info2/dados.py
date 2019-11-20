e479aafb54dd5eff9a6bc87acd8be04aa0f52e7b
Pedorgas <50590716+Pedorgas@users.noreply.github.com>
2019-11-20 13:16:50 -0300

    Provas E Arquivos Python 1Info2 (do predo)
    estou pronto pra fechar o ano

---
+++ Provas e Arquivos Python 1Info2/dados.py
@@ -0,0 +1,23 @@
+from random import randint  # Importa a funcao de gerar numeros aleatorios
+
+print("hamburger")
+print("jogo do dado")
+
+dado1 = randint(1, 6)   # numero aleatorio entre 1 e 6
+dado2 = randint(1, 6)   # numero aleatorio entre 1 e 6
+print('Dado 1=', dado1 + dado2)
+
+dado3 = randint(1, 6)   # numero aleatorio entre 1 e 6
+dado4 = randint(1, 6)   # numero aleatorio entre 1 e 6
+print('Dado 3=', dado3 + dado4)
+
+P1 = dado1+dado2
+
+P2 = dado3+dado4
+
+if P1 > P2:
+    print("P1 win")
+elif P2 > P1:
+    print("P2 win")
+else:
+    print("draw")
